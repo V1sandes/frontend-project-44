3c8159c3ca5cea37838451cc6c513a1e60f6bb6f
V1sandes <egorsizyh81@gmail.com>
2025-12-11 22:47:43 +0700

    Update brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,28 +1,5 @@
-import startEngine from './index.js';
+#!/usr/bin/env node
 
-const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
-const getRandomNum = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
-const limit = {
-  min: 0,
-  max: 100,
-};
+import startEvenGame from '../src/brain-even.js';
 
-const isEven = (num) => num % 2 === 0;
-
-const getEvenGameOptions = () => {
-  const randNum = getRandomNum(limit.min, limit.max);
-
-  const expression = `${randNum}`;
-
-  const answer = isEven(randNum) ? 'yes' : 'no';
-
-  return {
-    answer,
-    expression,
-  };
-};
-
-const startEvenGame = () => startEngine(rule, getEvenGameOptions);
-
-export default startEvenGame;
-//
+startEvenGame();
