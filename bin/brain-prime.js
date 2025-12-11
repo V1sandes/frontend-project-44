aec6c54de6baf12643e1b3ffea1d55ecf89a675a
V1sandes <egorsizyh81@gmail.com>
2025-12-11 22:48:43 +0700

    Update brain-prime.js

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,29 +1,5 @@
-import startEngine from './index.js';
+#!/usr/bin/env node
 
-const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-const limit = {
-  min: 0,
-  max: 1000,
-};
+import runBrainPrimeGame from '../src/brain-prime.js';
 
-const isPrime = (num) => {
-  for (let i = 2, s = Math.sqrt(num); i <= s; i += 1) {
-    if (num % i === 0) return false;
-  }
-  return num > 1;
-};
-
-const getQuAndAn = () => {
-  const expression = getRandomNumber(limit.min, limit.max);
-  const answer = isPrime(expression) ? 'yes' : 'no';
-
-  return {
-    answer,
-    expression,
-  };
-};
-
-const runBrainPrimeGame = () => startEngine(rule, getQuAndAn);
-
-export default runBrainPrimeGame;
+runBrainPrimeGame();
