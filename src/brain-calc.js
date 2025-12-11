96e41bf7cf4a494ad1368279fac3309f17024d63
V1sandes <egorsizyh81@gmail.com>
2025-12-11 22:42:45 +0700

    Create brain-calc.js

---
+++ src/brain-calc.js
@@ -0,0 +1,48 @@
+import startEngine from './index.js';
+
+const rule = 'What is the result of the expression?';
+const getRandomNum = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+const limit = {
+  min: 0,
+  max: 100,
+};
+const operator = {
+  plus: '+',
+  minus: '-',
+  prod: '*',
+};
+
+const calculateExpr = (firstNum, secondNum, oper) => {
+  switch (oper) {
+    case operator.plus:
+      return firstNum + secondNum;
+    case operator.minus:
+      return firstNum - secondNum;
+    case operator.prod:
+      return firstNum * secondNum;
+    default:
+      return (`Unknown '${oper}'!`);
+  }
+};
+
+const getCalcGameOptions = () => {
+  const randA = getRandomNum(limit.min, limit.max);
+  const randB = getRandomNum(limit.min, limit.max);
+  const randOper = Object.values(operator)[getRandomNum(0, Object.keys(operator).length - 1)];
+
+  const expression = `${randA} ${randOper} ${randB}`;
+
+  const value = calculateExpr(randA, randB, randOper);
+
+  const answer = String(value);
+
+  return {
+    answer,
+    expression,
+  };
+};
+
+const startCalcGame = () => startEngine(rule, getCalcGameOptions);
+
+export default startCalcGame;
+//
